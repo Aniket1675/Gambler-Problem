7233f4be508a360a3e66f7bf8d3573993e62ec65
Aniket1675 <Aniket@05>
2022-03-15 11:44:06 +0530

    Added UC1 for Declaration of variables

---
+++ src/com/bridgelabz/gambler/GamblerProblem.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.gambler;
+
+public class GamblerProblem
+{
+    //Use case 1
+    public static final int INITIAL_STAKE = 100;
+    public static final int BET_AMT = 1;
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
